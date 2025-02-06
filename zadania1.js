058672bbb427ad65217ce726382d8c270429dea9
Kamil <kamils3542@gmail.com>
2025-02-06 13:41:00 +0100

    second-commit

--- zadania1.js
+++ zadania1.js
@@ -1,5 +1,22 @@
-// let companies = []
-// for(i = 0; i < 5; i++){
-//     let company = prompt('Podaj firmy it', '')
-//     companies.push(company)
-// }
+first_case = {
+    name: 'first_case',
+    created_task: new Date('2021-10-26T19:48:12+00:00'),
+    end_task: null
+}
+second_case = {
+    name: 'second_case',
+    created_task: new Date('2021-09-26T19:48:12+00:00'),
+    end_task: new Date('2021-10-26T19:48:12+00:00')
+}
+function retrieveSeconds(obj){
+    const created = obj.created_task.getTime() / 1000
+    if(obj.end_task == null || obj.end_task == 'None') {
+        console.log("błąd w obiekcie")
+        return
+    }
+
+    const ended = obj.end_task.getTime() / 1000
+    return ended - created;
+}
+
+retrieveSeconds(second_case)
